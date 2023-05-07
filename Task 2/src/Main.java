392017fce337e9a0e362a73e5217290564918e06
MansurPoludov <mansurpoludov2@gmail.com>
2023-05-07 12:17:53 +0300

    add a cycle for and desided a problem

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
+        for(int i = 1 ; i <= 100 ; i++) {
+            if (i % 2 == 0)
+            {
+                System.out.println( i + " suck , go to find new job uahahahaha");
+            }
+        }
         //todo Тут увольняем айтишников
     }
 }
