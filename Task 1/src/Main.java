ce0d78f2cdfa5e297738330688c4ec6d8bb35b5b
MansurPoludov <mansurpoludov2@gmail.com>
2023-05-07 11:50:19 +0300

    добавил условия и дописал код

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
         int ghoulTearsCount = 0;        //Слезы вурдалака
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
-
+        boolean isCorrect = false;
         //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.print( "Введите количество жабьих глаз : ");
+        toadEyesCount = new Scanner(System.in).nextInt();
+        System.out.print( "Введите количество Слезы вурдалака: ");
+        ghoulTearsCount  = new Scanner(System.in).nextInt();
+        System.out.print( "Введите количество Кости ворона: ");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+        System.out.print( "Введите количество Пельмени: ");
+        dumplingsCount = new Scanner(System.in).nextInt();
+        if (toadEyesCount >= 3 && ravenBonesCount >= 1){
+            System.out.println("ВЫ можете приготовить зелье зоркости");
+
+            isCorrect = true;
+        }
+        if ( ravenBonesCount >= 2 && dumplingsCount >= 4 ){
+            System.out.println("ВЫ можете приготовить зелье стойкости ");
+            isCorrect = true;
+        }
+        if ( ghoulTearsCount >= 7 && dumplingsCount >= 1 && toadEyesCount >= 2){
+            System.out.println("ВЫ можете приготовить Эликсир скрытности ");
+            isCorrect = true;
+        }
+        if ( ghoulTearsCount >= 5 && dumplingsCount >= 10 && toadEyesCount >= 4){
+            System.out.println("ВЫ можете приготовить Запретный эликсир");
+            isCorrect = true;
+        }
+        if (!isCorrect) {
+            System.out.print("Недостаточно ингридиентов");
+
+        }
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
